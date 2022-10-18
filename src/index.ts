9dfe8b4d38cef4d37ab9dd0f77342b7c6b3f8202
Regis Kopper <kopper@uncg.edu>
2022-10-18 14:29:52 -0400

    Added comments and implemented thumbstick "reel"

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-/* CSCI 5619 Lecture 12, Fall 2020
+/* CSC-495 Virtual Reality Raycasting, Fall 2022
+ * Author: Regis Kopper
+ *
+ * Based on
+ * CSC 5619, Fall 2020
  * Author: Evan Suma Rosenberg
+ * 
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  */ 
 
@@ -25,7 +30,6 @@ import "@babylonjs/core/Helpers/sceneHelpers";
 
 // Import debug layer
 import "@babylonjs/inspector";
-import { colorCorrectionPixelShader } from "@babylonjs/core/Shaders/colorCorrection.fragment";
 
 class Game 
 { 
@@ -119,29 +123,44 @@ class Game
         xrHelper.teleportation.dispose();
         xrHelper.pointerSelection.dispose();
 
+        /* This is how you can use raycasting without a custom implementation
+           The benefit of a custom implementation is that you can modify the behavior
+           of the interaction technique so it best suits your needs.
+        // Register event handler for selection events (pulling the trigger, clicking the mouse button)
+        this.scene.onPointerObservable.add((pointerInfo) => {
+            this.processPointer(pointerInfo);
+        });
+        */
+
+        // Create points for the laser pointer
         var laserPoints = [];
         laserPoints.push(Vector3.Zero());
         laserPoints.push(new Vector3(0, 0, 20));
 
+        // Create a laser pointer and make sure it is not pickable
         this.laserPointer = MeshBuilder.CreateLines("laserPointer", {points: laserPoints}, this.scene);
         this.laserPointer.color = Color3.Yellow();
         this.laserPointer.alpha = .5;
         this.laserPointer.visibility = 0;
         this.laserPointer.isPickable = false;
 
+        // This transform will be used to attach objects to the laser pointer
         this.selectionTransform = new TransformNode("selectionTransform", this.scene);
         this.selectionTransform.parent = this.laserPointer;
         
+        // Create a blue emissive material
         var cubeMaterial = new StandardMaterial("blueMaterial", this.scene);
         cubeMaterial.diffuseColor = new Color3(.284, .73, .831);
         cubeMaterial.specularColor = Color3.Black();
         cubeMaterial.emissiveColor = new Color3(.284, .73, .831);
 
+        // Create a test cube at a convenient place
         // var testCube = MeshBuilder.CreateBox("testCube", {size: .25}, this.scene);
         // testCube.position = new Vector3(.6, 1.5, 2);
         // testCube.material = cubeMaterial;
         // testCube.edgesWidth = .3;
 
+        // Create a 3D selection and manipulation testbed
         for(let i=0; i < 100; i++)
         {
             let cube = MeshBuilder.CreateBox("cube", {size: Math.random() * .3 + .1}, this.scene);
@@ -150,12 +169,13 @@ class Game
             cube.edgesWidth = .3;
         }
 
+        // Attach the laser pointer to the right controller when it is connected
         xrHelper.input.onControllerAddedObservable.add((inputSource) => {
             if(inputSource.uniqueId.endsWith("right"))
             {
                 this.rightController = inputSource;
                 this.laserPointer!.parent = this.rightController.pointer;
-                this.laserPointer!.visibility = 1
+                this.laserPointer!.visibility = 1;
             }
             else
             {
@@ -163,26 +183,65 @@ class Game
             }
         });
 
+        // Don't forget to deparent the laser pointer or it will be destroyed!
         xrHelper.input.onControllerRemovedObservable.add((inputSource) => {
-            this.laserPointer!.parent = null;
-            this.laserPointer!.visibility = 0;
+
+            if(inputSource.uniqueId.endsWith("right")) 
+            {
+                this.laserPointer!.parent = null;
+                this.laserPointer!.visibility = 0;
+            }
         });
 
 
         this.scene.debugLayer.show(); 
     }
 
+    /* Here is the observable handler for the default WebXR raycasting
+       implementation.
+    // Event handler for processing pointer selection events
+    private processPointer(pointerInfo: PointerInfo)
+    {
+        switch (pointerInfo.type) {
+
+            case PointerEventTypes.POINTERDOWN:
+
+                if(this.selectedObject)
+                {
+                    this.selectedObject.disableEdgesRendering();
+                    this.selectedObject = null;
+                }
+
+                // if an object was hit
+                if(pointerInfo.pickInfo?.hit)
+                {
+                    this.selectedObject = pointerInfo.pickInfo.pickedMesh;
+                    this.selectedObject!.enableEdgesRendering();
+                }
+            break;
+        }
+    }
+    */
+
+
     // The main update loop will be executed once per frame before the scene is rendered
     private update() : void
     {
+        // Polling for controller input
         this.processControllerInput();
     }
 
+    // Process event handlers for controller input
     private processControllerInput()
     {
+        //select the object when it intersects with the right trigger
         this.onRightTrigger(this.rightController?.motionController?.getComponent("xr-standard-trigger"));
+        
+        //use a "reel" technique to manipulate the distance of the object by pushing the thumbstick
+        this.onRightThumbstick(this.rightController?.motionController?.getComponent("xr-standard-thumbstick"));
     }
 
+    // Attach the object to the ray when trigger is pressed, and dettach it when the trigger is released
     private onRightTrigger(component?: WebXRControllerComponent)
     {
         if(component?.changes.pressed)
@@ -194,25 +253,30 @@ class Game
                 var ray = new Ray(this.rightController!.pointer.position, this.rightController!.pointer.forward, 20);
                 var pickInfo = this.scene.pickWithRay(ray);
 
+                // Deselect the currently selected object 
                 if(this.selectedObject)
                 {
-                    this.selectedObject?.disableEdgesRendering();
+                    this.selectedObject!.disableEdgesRendering();
                     this.selectedObject = null;
                 }
 
+                // If an object was hit, select it
                 if(pickInfo?.hit)
                 {
                     this.selectedObject = pickInfo!.pickedMesh;
                     this!.selectedObject!.enableEdgesRendering();
 
+                    // Parent the object to the transform on the laser pointer
                     this.selectionTransform!.position = new Vector3(0, 0, pickInfo.distance);
                     this.selectedObject!.setParent(this.selectionTransform);
                 }
             }
             else
             {
-                this.laserPointer!.color = Color3.Yellow();
+                // Reset the laser pointer color
+                this.laserPointer!.color = Color3.Blue();
     
+                // Release the object from the laser pointer
                 if(this.selectedObject)
                 {
                     this.selectedObject!.setParent(null);
@@ -221,6 +285,22 @@ class Game
         }
     }
 
+    // Implement a "reel technique to manipulate the object's depth"
+    private onRightThumbstick(component?: WebXRControllerComponent)
+    {
+        // If we have an object that is currently attached to the laser pointer
+        // and the thumbstick was pushed
+        if(component?.changes.axes && this.selectedObject && this.selectedObject.parent)
+        {
+            // Use delta time to calculate the proper speed
+            // DeltaTime is the time, in milliseconds, between 
+            // the current and the last frame
+            var moveDistance = -component.axes.y * (this.engine.getDeltaTime() / 1000) * 3;
+
+            // Translate the object along the depth ray in world space
+            this.selectedObject.translate(this.laserPointer!.forward, moveDistance, Space.WORLD);
+        }
+    }
 
 }
 /******* End of the Game class ******/   
